a39b4faac778d5d4c0f9ba1a738866ad2359c6b4
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2023-11-04 02:00:07 -0300

    preparacion de las rutas

---
+++ routes/hello.ts
@@ -0,0 +1,10 @@
+// hello.ts
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Ruta de verificacion de funcionamiento, metodo GET funcionando");
+});
+
+export default router;
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import helloRoute from "../routes/hello";
+require("dotenv").config({ path: ".env" });
+
+const cors = require("cors");
+
+const DataBaseConnection = async () => {
+  try {
+    if (!process.env.DB_URL) {
+      throw new Error("La variable de entorno DB no está definida.");
+    }
+
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Base de datos conectada");
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+  }
+};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 3000;
+DataBaseConnection();
+app.use("/", helloRoute);
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
