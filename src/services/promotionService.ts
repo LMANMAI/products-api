50a4fc206ba6bc149c7241e148e266b3b37c30bf
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2024-02-08 17:17:17 -0300

    creacion de promociones

--- src/services/promotionService.ts
+++ src/services/promotionService.ts
@@ -1,8 +1,25 @@
 import Promotion from "../models/promotionModel";
+import Products from "../models/index";
 
 async function findAllPromotions() {}
 
 async function createNewPromotion(afectedProduct: any, discountAmount: number) {
+  let products = await Products.find();
+  //me traigo los productos
+  if (afectedProduct) {
+    const filters = Object.entries(afectedProduct);
+    products = products.filter((sneaker: any) =>
+      filters.every(([key, value]) => {
+        if (typeof value === "string" && sneaker[key]) {
+          return sneaker[key].toUpperCase().includes(value.toUpperCase());
+        }
+        return true;
+      })
+    );
+  }
+  //modifico el precio de los productos y le asigno un valor que sea modified:true
+  //guardo la promocion
+
   const newPromotion = new Promotion({
     afectedProduct,
     discountAmount,
